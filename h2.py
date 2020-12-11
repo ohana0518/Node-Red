9f7dd209dffe4235298af967c0fb1cec19b8975c
ohana0518 <71748162+ohana0518@users.noreply.github.com>
2020-12-11 17:44:21 +0800

    Create h2.py

---
+++ h2.py
@@ -0,0 +1,24 @@
+[
+    {
+        "id": "422b2368.921b7c",
+        "type": "http request",
+        "z": "80e96321.c2074",
+        "name": "",
+        "method": "GET",
+        "ret": "txt",
+        "paytoqs": false,
+        "url": "http://api.mediatek.com/mcs/v2/devices/DBr1jJuH/datachannels/ohana/datapoints.csv",
+        "tls": "",
+        "persist": false,
+        "proxy": "",
+        "authType": "",
+        "x": 300,
+        "y": 280,
+        "wires": [
+            [
+                "ea52d4c.ceac028",
+                "5dd11c23.609774"
+            ]
+        ]
+    }
+]
